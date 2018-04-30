0f220dc70129260cb93dcd3f85074ce8b6a5a842
Olivier Berger <olivier.berger@telecom-sudparis.eu>
2018-04-30 22:35:50 +0200

    Run labtainers from just a special "master/controller" container
    The Dockerfile.labtainer.master Dockerfile defines the necessary
    bits for running an installation of Labtainers inside a particular
    container named 'docker0' on purpose (hack)
    
    It contains a copy of labtainer.tar copied from distrib/
    
    By running Docker out of Docker (bind mounting the docker socket), this
    seems to be working, provided the patch on getDocker0IPAddr().
    
    I've tested it on the telnetlab lab :-) Only as student. Didn't attempt
    to grade ;)
    
    This is just a hack for the moment as a PoC.

---
+++ scripts/designer/bin/create_master_image.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+source ./set_reg.sh
+if [[ "$1" != -f ]]; then
+   echo "This will build the labtainer master controller image.  "
+   echo "registry is $LABTAINER_REGISTRY"
+   read -p "Continue? (y/n)"
+   if [[ ! $REPLY =~ ^[Yy]$ ]]
+   then
+       echo exiting
+       exit
+   fi
+else
+   echo "registry is $LABTAINER_REGISTRY"
+fi
+here=`pwd`
+cd ../
+cp ../../distrib/labtainer.tar ./
+
+cat <<EOT >bashrc.labtainer.master
+   if [[ ":\$PATH:" != *":./bin:"* ]]; then 
+       export PATH="\${PATH}:./bin:/home/labtainer/labtainer/trunk/scripts/designer/bin"
+       export LABTAINER_DIR=/home/labtainer/labtainer/trunk
+   fi
+EOT
+
+cp ../../scripts/labtainer-student/bin/labutils.py ./
+
+docker build --build-arg DOCKER_GROUP_ID="$(getent group docker | cut -d: -f3)" -f base_dockerfiles/Dockerfile.labtainer.master -t labtainer.master:latest .
+cd $here
